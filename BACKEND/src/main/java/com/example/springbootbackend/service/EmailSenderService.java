d47f5935ef3349c788ba700fdc6077eca49e6ffd
tuncAksuna <aksuna.tunc@gmail.com>
2021-10-03 00:03:30 +0300

    Add more informations to mail,send mail with attachments ( delete actions )

--- BACKEND/src/main/java/com/example/springbootbackend/service/EmailSenderService.java
+++ BACKEND/src/main/java/com/example/springbootbackend/service/EmailSenderService.java
@@ -2,11 +2,13 @@ package com.example.springbootbackend.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
 @Service
@@ -18,18 +20,23 @@ public class EmailSenderService {
 
   public void sendEmail(String toEmail, String subjectOfMail, String bodyOfEmail, Date sentTime) {
 
-    SimpleMailMessage mailMessage = new SimpleMailMessage();
-
-    mailMessage.setFrom("aksuna.tunc@gmail.com");
-    mailMessage.setTo(toEmail);
-    mailMessage.setText(bodyOfEmail);
-    mailMessage.setSubject(subjectOfMail);
-    mailMessage.setSentDate(sentTime);
+    MimeMessage mimeMessage = mailSender.createMimeMessage();
 
     try {
+      MimeMessageHelper mimeHelper = new MimeMessageHelper(mimeMessage, true);
+
+      mimeHelper.setFrom("aksuna.tunc@gmail.com");
+      mimeHelper.setTo(toEmail);
+      mimeHelper.setText(bodyOfEmail);
+      mimeHelper.setSubject(subjectOfMail);
+      mimeHelper.setSentDate(sentTime);
+
+      FileSystemResource file = new FileSystemResource("C:\\Users\\netadmin\\Desktop\\SPRING and JAVA\\PROJELER\\full-stack-app\\BACKEND\\src\\main\\java\\assets\\employee-deleted.png");
+      mimeHelper.addAttachment(file.getFilename(), file);
       log.trace("The mail succesfully sent to [{}]", toEmail);
-      mailSender.send(mailMessage);
-    } catch (MailException ex) {
+      mailSender.send(mimeMessage);
+
+    } catch (MessagingException ex) {
       log.warn("The mail has not been sent to [{}], sent time : [{}]", toEmail, sentTime);
       System.out.println(ex.getMessage());
     }
