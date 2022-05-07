8685c10a8e53022072ad66e0b3d4d68c8819a7bf
tuncAksuna <aksuna.tunc@gmail.com>
2022-05-07 13:30:35 +0300

    @Slf4j used in main class

--- BACKEND/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ BACKEND/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -1,6 +1,7 @@
 package com.example.springbootbackend;
 
 import com.example.springbootbackend.service.implemantations.EmailSenderService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@Slf4j
 public class SpringbootBackendApplication {
-
-  private static final Logger logger = LoggerFactory.getLogger(SpringbootBackendApplication.class);
 
   @Autowired
   private EmailSenderService senderService;
 
   public static void main(String[] args) {
     SpringApplication.run(SpringbootBackendApplication.class, args);
-    logger.info("APPLICATION IS RUNNING");
+    log.info("THE APPLICATION IS BEING RAN !");
   }
 
 }
